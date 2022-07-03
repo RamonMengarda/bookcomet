3f81600a2baf5047cc1b56503549bc95a513245e
RamonMengarda <ramon_hiago@hotmail.com>
2022-07-03 10:02:18 -0300

    frontend minimal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+class App extends Component {
+    state = {
+      books: []
+    };
+  
+    async componentDidMount() {
+      const response = await fetch('/books');
+      const body = await response.json();
+      this.setState({books: body});
+    }
+  
+    render() {
+      const {books} = this.state;
+      return (
+          <div className="App">
+            <header className="App-header">
+              <div className="App-intro">
+                <h2>Books</h2>
+                {books.map(book =>
+                    <div key={book.id}>
+                      {book.name} ({book.email})
+                    </div>
+                )}
+              </div>
+            </header>
+          </div>
+      );
+    }
+  }
+  export default App;
